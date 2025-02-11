80c6799458c92eaaa967615e6dd267611f579bbd
jonah <jonah.softwaredev@gmail.com>
2025-02-11 13:23:24 +0300

    perf: change logic of operate() func and event listener

--- calculator.js
+++ calculator.js
@@ -14,94 +14,59 @@ function divide(number1, number2) {
     return number1 / number2
 }
 
-let num1 = 0
-let num2 = null
-let operator = null
-let total = null
 function operate(num1, num2, operator) {
-    if (num1 !== null) {
-        total = num1
-    }
-
-    total = parseInt(total)
+    let total = null
+    num1 = parseInt(num1)
     num2 = parseInt(num2)
     
-
     switch (operator) {
         case "+":
-            total = add(total, num2)
+            total = add(num1, num2)
             break
         case "-":
-            total = subtract(total, num2)
+            total = subtract(num1, num2)
             break
         case "*":
-            total = multiply(total, num2)
+            total = multiply(num1, num2)
             break
         case "/":
-            total = divide(total, num2)
+            total = divide(num1, num2)
             break
     }
 
-    console.log(total)
+    return total
 }
 
-let calc = document.querySelector(".numWithOperators")
-let operand = ""
-let values = []
-let clickInput = ""
-let op = []
-const operationSigns = ["+", "-", "/", "*"]
-calc.addEventListener("click", e => {
-    let target = e.target.id
-    
-    if (target != "=" && !operationSigns.includes(target)) {
-        clickInput += target
-    } else {
-        if (clickInput) values.push(clickInput)
-        console.log(values)
-        clickInput = ""
+const buttons = document.querySelector(".numWithOperators")
+let index = 0
+let number = []
+let operator = []
+let result = null
+const OPERATORS = ["+", "-", "*", "/", "="]
 
-        if (target != "=") {
-            op.push(target)
-            
+buttons.addEventListener("click", event => {
+    let target = event.target.id
+
+    if (!OPERATORS.includes(target)) {
+        if (number[index] === undefined) number[index] = target
+        else number[index] += target
+    } else if (number.length >= 1) {
+        if (number.length >= 2) {
+            if (!result) {
+                result = operate(number[0], number[index], operator.at(-1))
+            } else {
+                result = operate(result, number[index], operator.at(-1))
+            }
+
+            console.log(result)
         }
         
+        index++
+        if (target !== "=") operator.push(target)
+        else {
+            result = null
+            number = []
+            index = 0
+        }
     }
-
-    if (target === "=" && values.length >= 1) {
-        if (values.length == 2) {
-            num1 = values[0]
-            num2 = values[1]
-            operator = op.at(-1)
-           
-        } else if (values.length == 1) {
-            num1 = null
-            num2 = values[0]
-            operator = op.at(-1)
-            console.log("Operation: " + operator)
-            
-        }
-        
-        console.log("equal sign")
-        values = []
-        operate(num1, num2, operator)
-        num1 = 0
-    } else if (operationSigns.includes(target)) {
-        if (num1 !== null && values.length >= 2) {
-            num1 = values[0]
-            num2 = values[1]
-            operator = op.at(-2)
-            values = []
-            console.log("not equal not null")
-            operate(num1, num2, operator)
-            num1 = null
-            
-        } else if (num1 === null && values.length >= 1) {
-            num2 = values[0]
-            operator = op.at(-2)
-            values = []
-            console.log('null')
-            operate(num1, num2, operator)
-        }    
-    } 
 })
